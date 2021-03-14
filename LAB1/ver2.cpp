cf8e5651a96e06633bfc5895760ba0a8ad8ecf09
polinaence <p.popova1@g.nsu.ru>
2021-03-14 20:37:33 +0700

    Изменила выделение памяти, теперь выделяется не в каждой итерации. Добавила copy и говорю, что она нужна для того, чтобы избежать неопределенного поведения при race condition Вынесла проверку на Rank. Вернула const на законное место. Убрала функцию, оборачивающую функцию

--- LAB1/ver2.cpp
+++ LAB1/ver2.cpp
@@ -1,17 +1,12 @@
 #include "mpi/mpi.h"
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 const float EPS = 0.00001f;
 const float TAU = 0.00001f;
 
-void copyFirstVectorToSecond(const float *vector1, float *vector2, int vectorSize) {
-    for (int i = 0; i < vectorSize; i++) {
-        vector2[i] = vector1[i];
-    }
-}
-
-void mul(const float *partA, const float *x, float *res, int matrixSize, int procRank, int procSize) {
+void mul(const float* partA, const float* x, float* res, int matrixSize, int procRank, int procSize, float* buffer) {
     int numberOfLines = matrixSize / procSize;
     int shift = procRank * numberOfLines;
     for (int i = 0; i < numberOfLines; ++i) {
@@ -20,66 +15,55 @@ void mul(const float *partA, const float *x, float *res, int matrixSize, int pro
             res[shift + i] += partA[i * matrixSize + j] * x[j];
         }
     }
-    float *buffer = new float[matrixSize];
 
     MPI_Allgather(res + shift, numberOfLines, MPI_FLOAT, buffer, numberOfLines, MPI_FLOAT, MPI_COMM_WORLD);
-    copyFirstVectorToSecond(buffer, res, matrixSize);
 
-    delete[] buffer;
+    std::copy(buffer, buffer + matrixSize, res);
 }
 
 
-float vectorAbs(const float *x, int size) {
+float vectorAbs(const float* x, int size) {
     float len = 0.0f;
     for (int i = 0; i < size; ++i)
         len += x[i] * x[i];
     return len;
 }
 
-void vectorSub(const float *x, const float *y, float *res, float k, int size) {
+void vectorSub(const float* x, const float* y, float* res, float k, int size) {
     for (int i = 0; i < size; ++i)
         res[i] = x[i] - k * y[i];
 }
 
 
-float *nextX(float *x, const float *b, float *Ax, int matrixSize) {
+float* nextX(float* x, const float* b, float* Ax, int matrixSize, float* buffer) {
     //Ax уже умноженный после проверки (... < EPS) - используеv его
-    float *y = new float[matrixSize];
-    vectorSub(Ax, b, y, 1, matrixSize);
+    vectorSub(Ax, b, buffer, 1, matrixSize);
+    vectorSub(x, buffer, x, TAU, matrixSize);
 
-    float *part_x_plus_one = new float[matrixSize];
-    vectorSub(x, y, part_x_plus_one, TAU, matrixSize);
-    copyFirstVectorToSecond(part_x_plus_one, x, matrixSize);
-    delete[] part_x_plus_one;
-
-    delete[] y;
     return x;
 }
 
-bool ending(float *x, float *partA, float *b, float *Ax, int matrixSize, int procRank, int procSize) {
-    mul(partA, x, Ax, matrixSize, procRank, procSize);
+bool ending(float* x, float* partA, float* b, float* Ax, int matrixSize, int procRank, int procSize, float* buffer) {
+    mul(partA, x, Ax, matrixSize, procRank, procSize, buffer);
 
-    float *sub = new float[matrixSize];
-    vectorSub(Ax, b, sub, 1, matrixSize);
+    vectorSub(Ax, b, buffer, 1, matrixSize);
 
     float div;
     if (procRank == 0) {
-        div = (vectorAbs(sub, matrixSize) / vectorAbs(b, matrixSize));
+        div = (vectorAbs(buffer, matrixSize) / vectorAbs(b, matrixSize));
     }
 
     MPI_Bcast(&div, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-    bool res = (div < EPS * EPS);
+    bool res = (div < EPS* EPS);
 
-//    if (procRank == 0) {
-//        std::cout << div << std::endl;
-//    }
-
-    delete[] sub;
+    if (procRank == 0) {
+        std::cout << div << std::endl;
+    }
 
     return res;
 }
 
-void init_1(float *&x, float *&partA, float *&b, float *&Ax, int matrixSize, int procRank, int procSize) {
+void init_1(float*& x, float*& partA, float*& b, float*& Ax, int matrixSize, int procRank, int procSize) {
 
     int startLine; //номер строки с которой процесс считает матрицу A
     int numberOfLines; //количествно строк которые обрабатывает каждый процесс
@@ -98,7 +82,7 @@ void init_1(float *&x, float *&partA, float *&b, float *&Ax, int matrixSize, int
 
     b = new float[matrixSize];
     for (int i = 0; i < matrixSize; ++i) {
-        b[i] = (float) matrixSize + 1;
+        b[i] = (float)matrixSize + 1;
     }
     x = new float[matrixSize];
     for (int i = 0; i < matrixSize; ++i) {
@@ -108,51 +92,44 @@ void init_1(float *&x, float *&partA, float *&b, float *&Ax, int matrixSize, int
     Ax = new float[matrixSize];
 }
 
-void printRes(float* x, int size, int procRank) {
-    if(procRank == 0) {
-        for(int i = 0; i < size; ++ i)
-            std::cout << x[i] << " ";;
-    }
+void printRes(float* x, int size) {
+    for (int i = 0; i < size; ++i)
+        std::cout << x[i] << " ";;
 }
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
 
     int size = 2 * 2048; //Размер матрицы и вектора
-    float *partA; //часть матрицы коэффицентов для каждого процесса она своя
-    float *b; //вектор правых частей
-    float *x; //вектор значений
+    float* partA; //часть матрицы коэффицентов для каждого процесса она своя
+    float* b; //вектор правых частей
+    float* x; //вектор значений
 
-    float *Ax; //вспомогательный вектор, хранит в себе результат умножения матрицы A на вектор x
+    float* Ax; //вспомогательный вектор, хранит в себе результат умножения матрицы A на вектор x
+
+    float* buffer = new float[size]; //вектор, который будет использоваться как буфер для промежуточных расчетов
 
     int procSize; //количество выполняемых процессов
     int procRank; //номер текущего процесса(нумерация с нуля)
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     MPI_Comm_size(MPI_COMM_WORLD, &procSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
 
     init_1(x, partA, b, Ax, size, procRank, procSize);
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     double startTime, endTime;
     if (procRank == 0) {
         startTime = MPI_Wtime();
     }
 
-    while (!ending(x, partA, b, Ax, size, procRank, procSize)) {
-        x = nextX(x, b, Ax, size);
+    while (!ending(x, partA, b, Ax, size, procRank, procSize, buffer)) {
+        x = nextX(x, b, Ax, size, buffer);
     }
 
     if (procRank == 0) {
         endTime = MPI_Wtime();
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    printRes(x, size, procRank);
-
-    if (procRank == 0) {
+        printRes(x, size);
         std::cout << "\nElapsed time is " << endTime - startTime << '\n';
     }
 
@@ -160,6 +137,7 @@ int main(int argc, char *argv[]) {
     delete[] b;
     delete[] x;
     delete[] Ax;
+    delete[] buffer;
 
     MPI_Finalize();
     return 0;
--- LAB1/ver3.cpp
+++ LAB1/ver3.cpp
@@ -1,14 +1,14 @@
 #include "mpi/mpi.h"
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 #include <unistd.h>
 
 const float EPS = 0.00001f;
-float TAU = 0.00001f;
-float lastDiv = 1000000.0f;
+const float TAU = 0.00001f;
 const float START_X = 0.0f;
 
-void printVector(const float *vector, int vectorSize, const std::string &vectorName) {
+void printVector(const float* vector, int vectorSize, const std::string& vectorName) {
 
     std::cout << vectorName;
     for (int i = 0; i < vectorSize; i++) {
@@ -17,13 +17,7 @@ void printVector(const float *vector, int vectorSize, const std::string &vectorN
     std::cout << std::endl;
 }
 
-void copyFirstVectorToSecond(const float *vector1, float *vector2, int vectorSize) {
-    for (int i = 0; i < vectorSize; i++) {
-        vector2[i] = vector1[i];
-    }
-}
-
-void mul(const float *partA, const float *x, float *res, int matrixSize, int procRank, int procSize) {
+void mul(const float* partA, const float* x, float* res, int matrixSize, int procRank, int procSize) {
 
     int numberOfLines = matrixSize / procSize;
     int startLine = (procRank) * (numberOfLines);
@@ -37,14 +31,14 @@ void mul(const float *partA, const float *x, float *res, int matrixSize, int pro
 
 }
 
-float vectorFullAbs(const float *x, int size) {
+float vectorFullAbs(const float* x, int size) {
     float len = 0.0f;
     for (int i = 0; i < size; ++i)
         len += x[i] * x[i];
     return len;
 }
 
-float vectorAbs(const float *x, int matrixSize, int procRank, int procSize) {
+float vectorAbs(const float* x, int matrixSize, int procRank, int procSize) {
     int numberOfLines = matrixSize / procSize;
     int startLine = (procRank) * (numberOfLines);
     float len = 0.0f;
@@ -54,7 +48,7 @@ float vectorAbs(const float *x, int matrixSize, int procRank, int procSize) {
     return len;
 }
 
-void vectorSub(const float *x, const float *y, float *res, float k, int matrixSize, int procRank, int procSize) {
+void vectorSub(const float* x, const float* y, float* res, float k, int matrixSize, int procRank, int procSize) {
 
     int numberOfLines = matrixSize / procSize;
     int startLine = (procRank) * (numberOfLines);
@@ -64,34 +58,18 @@ void vectorSub(const float *x, const float *y, float *res, float k, int matrixSi
 
 }
 
-void nextX(float *x, const float *b, float *Ax, int matrixSize, int procRank, int procSize) {
-    //Ax - b уже после проверки (... < EPS) - используем его
-    //vectorSub(Ax, b, Ax, 1, matrixSize, procRank, procSize);
-    vectorSub(x, Ax, x, TAU, matrixSize, procRank, procSize);
-}
-
-bool ending(float *x, float *partA, float *b, float *Ax, int matrixSize, int procRank, int procSize) {
+bool ending(float* x, float* partA, float* b, float* Ax, int matrixSize, int procRank, int procSize, float* buffer) {
     mul(partA, x, Ax, matrixSize, procRank, procSize); // частичная Ax
-
-    //float *firstTemp = (float*) calloc(matrixSize, sizeof(float));
 
     vectorSub(Ax, b, Ax, 1, matrixSize, procRank, procSize); // частичная Ax-b
 
-    float *tempBuf = new float[matrixSize];
+    MPI_Allreduce(Ax, buffer, matrixSize, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD); // Собираем Ax - b
 
-    MPI_Allreduce(Ax, tempBuf, matrixSize, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD); // Собираем Ax - b
-
-    copyFirstVectorToSecond(tempBuf, Ax, matrixSize);
+    std::copy(buffer, buffer + matrixSize, Ax);
 
     float absOfAxb;
-    //std::cout << procRank << " ";
-    //printVector(Ax, 10, "First elements Ax-b not collected: ");
     if (procRank == 0) {
-        //printVector(firstTemp, 10, "First elements Ax-b not collected: ");
-        //printVector(tempBuf, 10, "First elements Ax-b collected: ");
-        absOfAxb = vectorFullAbs(tempBuf, matrixSize); // считаем ||Ax - b|| ^ 2
-        //std::cout << "Abs of AX - B " << absOfAxb << std::endl;
-        //printVector(tempBuf, matrixSize, "||Ax - b||: ");
+        absOfAxb = vectorFullAbs(buffer, matrixSize); // считаем ||Ax - b|| ^ 2
     }
 
     float partialAbs = vectorAbs(b, matrixSize, procRank, procSize); // считаем bk^2 + .. + bm^2
@@ -103,27 +81,19 @@ bool ending(float *x, float *partA, float *b, float *Ax, int matrixSize, int pro
     }
     MPI_Bcast(&div, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    bool res = (div < EPS * EPS);
-    if (div > lastDiv) {
-        if (procRank == 0) {
-            std::cout << "Reverse TAU" << std::endl;
-        }
-        TAU = TAU * -1.0f;
-    }
+    bool res = (div < EPS* EPS);
 
     if (procRank == 0) {
-        std::cout << div << " " << lastDiv << std::endl;
+        std::cout << div << std::endl;
         std::cout << "==================================" << std::endl;
     }
     //usleep(1000000);
     //MPI_Barrier(MPI_COMM_WORLD);
 
-    lastDiv = div;
-
     return res;
 }
 
-void init_1(float *&x, float *&partA, float *&b, float *&Ax, int matrixSize, int procRank, int procSize) {
+void init_1(float*& x, float*& partA, float*& b, float*& Ax, int matrixSize, int procRank, int procSize) {
 
     int startLine; //номер строки с которой процесс считает матрицу A
     int numberOfLines; //количествно строк которые обрабатывает каждый процесс
@@ -146,7 +116,7 @@ void init_1(float *&x, float *&partA, float *&b, float *&Ax, int matrixSize, int
     for (int i = 0; i < matrixSize; ++i) {
         b[i] = 0.0f;
         if (i >= startLine && i < startLine + numberOfLines) {
-            b[i] = (float) matrixSize + 1;
+            b[i] = (float)matrixSize + 1;
         }
     }
     x = new float[matrixSize];
@@ -160,51 +130,44 @@ void init_1(float *&x, float *&partA, float *&b, float *&Ax, int matrixSize, int
     Ax = new float[matrixSize];
 }
 
-void printRes(float *x, int size, int procRank) {
-    if (procRank == 0) {
-        for (int i = 0; i < size; ++i)
-            std::cout << x[i] << " ";;
-    }
+void printRes(float* x, int size) {
+    for (int i = 0; i < size; ++i)
+        std::cout << x[i] << " ";;
 }
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
 
     int size = 2048 * 2; //Размер матрицы и вектора
-    float *partA; //часть матрицы коэффицентов для каждого процесса она своя
-    float *b; //вектор правых частей
-    float *x; //вектор значений
+    float* partA; //часть матрицы коэффицентов для каждого процесса она своя
+    float* b; //вектор правых частей
+    float* x; //вектор значений
 
-    float *Ax; //вспомогательный вектор, хранит в себе результат умножения матрицы A на вектор x
+    float* Ax; //вспомогательный вектор, хранит в себе результат умножения матрицы A на вектор x
 
     int procSize; //количество выполняемых процессов
     int procRank; //номер текущего процесса(нумерация с нуля)
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    float* buffer = new float[size]; //вектор, который будет использоваться как буфер для промежуточных расчетов
 
     MPI_Comm_size(MPI_COMM_WORLD, &procSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
 
     init_1(x, partA, b, Ax, size, procRank, procSize);
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     double startTime, endTime;
     if (procRank == 0) {
         startTime = MPI_Wtime();
     }
 
-    while (!ending(x, partA, b, Ax, size, procRank, procSize)) {
-        nextX(x, b, Ax, size, procRank, procSize);
+    while (!ending(x, partA, b, Ax, size, procRank, procSize, buffer)) {
+        vectorSub(x, Ax, x, TAU, size, procRank, procSize);
     }
 
     if (procRank == 0) {
         endTime = MPI_Wtime();
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    printRes(x, size, procRank);
-
-    if (procRank == 0) {
+        printRes(x, size);
         std::cout << "\nElapsed time is " << endTime - startTime << '\n';
     }
 
@@ -212,6 +175,7 @@ int main(int argc, char *argv[]) {
     delete[] b;
     delete[] x;
     delete[] Ax;
+    delete[] buffer;
 
     MPI_Finalize();
     return 0;
